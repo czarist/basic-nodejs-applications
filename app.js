25059b261b685779131f44962f1b79fe87d51ba9
Cezar  Trentin <czartrentin@gmail.com>
2022-02-09 14:46:54 -0300

    reestruturando projeto

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
-var express = require('express');
-var app = express();
-app.set('view engine', 'ejs');
 
+
+var app = require('./config/server');
 
 app.get('/', function (req, res) {
-    res.send("<html><body>Portal de Noticias</body></html>")
+    res.render("home/index");
+})
+
+app.get('/tecnologia', function (req, res) {
+    res.render("secao/tecnologia");
+})
+
+app.get('/formulario_inclusao_noticia', function (req, res) {
+    res.render("admin/form_add_noticia");
+})
+
+app.get('/noticias', function (req, res) {
+    res.render("noticias/noticia");
 })
 
 app.listen(3000, function () {
-    console.log("servidor rodando com express")
-})
+    console.log('servidor ON')
+}) 
